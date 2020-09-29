5bd0e0743dafcaa72b153265cfadcdfd2b14e178
Roger Colquehuanca Calcina <rogcolquehuancac@upt.pe>
2020-09-28 23:47:07 -0500

    Adde basic bot integration

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
-const express= require('express')
+const express = require('express')
 // const morgan = require('morgan')
 // const createError = require('http-errors')
-
+const bodyParser= require('body-parser');
 // require('dotenv').config()
+const TELEGRAM_API_TOKEN = process.env.TELEGRAM_API_TOKEN || ''
 
+const app = express()
 
-const app= express()
+app.use(bodyParser.json())
 // app.use(morgan('dev'))
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 
 
 app.get('/', async (req, res, next) => {
     res.send('Hello Wrld')
-  })
-  
+})
+
+app.post('/webhooks/telegram', (req, res, next) => {
+    console.log(req.body)
+    res.send({status:"ok"})
+});
+//https://telegram-bot-api-hunter.herokuapp.com/webhooks/telegram
 
 // app.use(async(req,res,next)=>{
 
@@ -22,12 +29,12 @@ app.get('/', async (req, res, next) => {
 //     next(createError.NotFound())
 // })
 
-app.use((err,req,res,next)=>{
+app.use((err, req, res, next) => {
     res.status(err.status || 500)
     res.send({
-        error:{
+        error: {
             status: err.status || 500,
-            message:err.message
+            message: err.message
         }
     })
 })
@@ -35,7 +42,7 @@ app.use((err,req,res,next)=>{
 
 const PORT = process.env.PORT || 3002
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
     console.log(`localhost:${PORT}`)
 })
